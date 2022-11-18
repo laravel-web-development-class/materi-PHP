bab028fd2927abae1d1628c50c9ade898d3ce9fc
Denna Mandela <denna.mandela13@gmail.com>
2022-11-18 21:36:00 +0700

    tipe data php

---
+++ introduction/tipe-data.php
@@ -0,0 +1,52 @@
+
+<?php
+// tipe data integer
+    $persegi = 5;
+    $luasPersegi = $persegi * $persegi;
+    echo "$luasPersegi";
+
+    $nilai = 50;
+    $nilai_kedua = 2;
+    $nilaiKetiga = 4;
+
+    echo "<br>";
+    echo "ini adalah angka $nilai_kedua";
+
+    // tipe data float
+    $nilai_float = 2.4;
+    $kedua_float = 1.5;
+
+    echo "<br>";
+    echo "$nilai_float";
+    echo "<br>";
+    echo "$kedua_float";
+    echo "<br>";
+
+    //tipe data string
+    $namaDepan = "Denna";
+    $namaBelakang = 'Mandela';
+
+    echo "<br>";
+    echo "ini adalah tipe data string";
+    echo "<br>";
+    echo "$namaDepan";
+    echo "<br>";
+    echo "$namaBelakang";
+    echo "<br>";
+
+    // tipe data boolean
+    $cekPertama = true;
+    $cekKedua = false;
+    
+    echo "ini adalah tipe data boolean";
+    echo "<br>";
+    echo "$cekPertama";
+    echo "<br>";
+
+    // tipe array
+    $merkMobil = [4, 3, 8];
+
+    echo "Menampilkan semua data array yang tersimpan di dalam variabel merkMobil";
+    echo "<br/>";
+    echo $merkMobil[1];
+?>
